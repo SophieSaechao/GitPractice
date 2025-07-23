fc95f42be7b488ec6de7e9552533c531ddad924c
Sophie <sunfloweryam@gmail.com>
2025-07-23 11:02:16 -0700

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Sheng Saechao on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
